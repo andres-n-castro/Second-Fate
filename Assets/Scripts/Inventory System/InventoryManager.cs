a24e8aab6705893f7ebda93fc53337f8f13aed2a
Andres Castro <160083621+andres-n-castro@users.noreply.github.com>
2026-02-27 16:47:16 -0500

    created a test item pick up object
    test item pick up object shows the process of using the scriptableObject script that purely holds data of in game item and then is added to the pickup script within the acutal item game object

--- Assets/Scripts/Inventory System/InventoryManager.cs
+++ Assets/Scripts/Inventory System/InventoryManager.cs
@@ -2,15 +2,42 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private InventoryManager Instance;
+    public GameObject inventoryMenu;
+    private bool inventoryMenuActivated;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        Instance = this;
+
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+
+        //later will change this logic to instead invoke the event in the UI manager to set the inventory UI to active
+        //but for right now we will set the inventory UI active logic in this script
+        if(Input.GetButtonDown("Open Inventory") && !inventoryMenuActivated)
+        {
+            Time.timeScale = 0f;
+            inventoryMenu.SetActive(true);
+            inventoryMenuActivated = true;
+        }
+        else if(Input.GetButtonDown("Open Inventory") && inventoryMenuActivated)
+        {
+            Time.timeScale = 1f;
+            inventoryMenu.SetActive(false);
+            inventoryMenuActivated = false;
+        }
     }
 }
---
+++ Assets/Scripts/Inventory System/ItemPickup.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    [SerializeField] private Item itemData;
+    void Start()
+    {
+        
+    }
+
+}
