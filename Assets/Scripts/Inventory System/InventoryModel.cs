b6f6d00f3a479bd18b763038ac38a558fe32b72c
Andres Castro <andresncastro12@gmail.com>
2026-02-28 20:18:08 -0500

    added inventory model

---
+++ Assets/Scripts/Inventory System/InventoryModel.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class InventoryModel : MonoBehaviour
+{
+    List<ItemSlot> inventorySlots;
+    public void AddItem()
+    {
+        
+    }
+
+    public void RemoveItem()
+    {
+        
+    }
+
+}
